b129179249059a801ef81640b35e0c5804bbfce2
Rahul P <Rahul.P@viacomcontractor.com>
2020-08-24 08:40:09 +0530

    Add uploadToS3 method

---
+++ vars/uploadToS3.groovy
@@ -0,0 +1,7 @@
+def call(String credentialsId, String bucketName, String folderToUpload){
+  withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${credentialsId}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+    sh "aws s3 ls"
+    sh 'ls'
+    sh "aws s3 cp ${folderToUpload} s3://${bucketName} --recursive"
+  }
+}
