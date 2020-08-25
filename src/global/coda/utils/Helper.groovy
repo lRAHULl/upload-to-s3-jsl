b9fdf3ec6dd969b327e741af8f87ed9e625937ff
Rahul P <Rahul.P@viacomcontractor.com>
2020-08-25 19:16:11 +0530

    Add uploadToS3 method

---
+++ src/global/coda/utils/Helper.groovy
@@ -0,0 +1,13 @@
+package global.coda.utils
+
+class Helper {
+
+  static void uploadToS3(String credentialsId, String bucketName, String folderToUpload){
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${credentialsId}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+      sh "aws s3 ls"
+      sh 'ls'
+      sh "aws s3 cp ${folderToUpload} s3://${bucketName} --recursive"
+    }
+  }
+
+}
--- vars/uploadToS3.groovy
+++ vars/uploadToS3.groovy
@@ -1,7 +1,5 @@
+import global.coda.utils.Helper
+
 def call(String credentialsId, String bucketName, String folderToUpload){
-  withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: "${credentialsId}", secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-    sh "aws s3 ls"
-    sh 'ls'
-    sh "aws s3 cp ${folderToUpload} s3://${bucketName} --recursive"
-  }
+  Helper.uploadToS3(credentialsId, bucketName, folderToUpload)
 }
